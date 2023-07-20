d0722ac89d47eb906ba5dd4b761093c5e1bcb078
xgm <153094236@qq.com>
2023-07-20 20:11:06 +0800

    admin站点管理 生成详情界面HTML界面

--- hzmeilan/apps/goods/admin.py
+++ hzmeilan/apps/goods/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 
 from . import models
-from celery_tasks.html.tasks import generate_static_list_search_html
+from celery_tasks.html.tasks import generate_static_list_search_html, generate_static_sku_detail_html
 
 
 class GoodsCategoryAdmin(admin.ModelAdmin):
@@ -24,6 +24,31 @@ class GoodsCategoryAdmin(admin.ModelAdmin):
         generate_static_list_search_html.delay()
 
 
+class SKUImageModelAdmin(admin.ModelAdmin):
+    """ 商品图片站点管理类 """
+
+    def save_model(self, request, obj, form, change):
+        """
+        admin保存图片
+        @param request: 请求体
+        @param obj: 图片模型对象
+        @param form:
+        @param change:
+        @return:
+        """
+        obj.save()
+        sku = obj.sku
+        if not sku.default_image:
+            sku.default_image = obj.image
+            sku.save()
+        generate_static_sku_detail_html(sku.id)
+
+    def delete_model(self, request, obj):
+        obj.delete()
+        sku = obj.sku
+        generate_static_sku_detail_html.delay(sku.id)
+
+
 # Register your models here.
 admin.site.register(models.GoodsCategory, GoodsCategoryAdmin)
 admin.site.register(models.GoodsChannelGroup)
@@ -31,7 +56,7 @@ admin.site.register(models.GoodsChannel)
 admin.site.register(models.Brand)
 admin.site.register(models.SPU)
 admin.site.register(models.SKU)
-admin.site.register(models.SKUImage)
+admin.site.register(models.SKUImage, SKUImageModelAdmin)
 admin.site.register(models.SPUSpecification)
 admin.site.register(models.SpecificationOption)
 admin.site.register(models.SKUSpecification)
--- hzmeilan/celery_tasks/html/tasks.py
+++ hzmeilan/celery_tasks/html/tasks.py
@@ -12,6 +12,7 @@ from django.template import loader
 from django.conf import settings
 
 from apps.goods.utils import get_categories
+from apps.goods.models import SKU
 
 
 @celery_app.task(name='generate_static_list_search_html')
@@ -25,5 +26,62 @@ def generate_static_list_search_html():
     html_text = template.render(context)
     file_path = os.path.join(settings.GENERATED_STATIC_HTML_FILES_DIR, 'list.html')
 
+    with open(file_path, 'w', encoding='utf-8') as f:
+        f.write(html_text)
+
+
+@celery_app.task(name='generate_static_sku_detail_html')
+def generate_static_sku_detail_html(sku_id):
+    """
+    生成静态商品页面
+    @param sku_id: 商品sku_id
+    @return:None
+    """
+    categories = get_categories()  # 商品分类菜单
+
+    sku = SKU.objects.get(id=sku_id)  # 获取当前sku的信息
+    sku.images = sku.skuimage_set.all()
+
+    goods = sku.category  # 面包屑导航信息频道
+    goods.channel = goods.cat1_spu.all()
+
+    sku_specs = sku.specs.order_by('spec_id')  # 构建当前商品规格键
+    sku_key = []
+    for spec in sku_specs:
+        sku_key.append(spec.option.id)
+
+    skus = goods.sku_set.all()  # 获取当前商品所有的sku
+
+    spec_sku_map = {}
+    for s in skus:  # 获取形成规格参数sku的字典键
+        s_specs = s.specs.order_by('spec_id')
+        key = []  # 用于形成规格参数sku字典的键
+        for spec in s_specs:
+            key.append(spec.option.id)
+        spec_sku_map[tuple(key)] = s.id  # 向规格参数sku字典添加记录
+
+    specs = goods.goodschannel_set.order_by('id')
+
+    if len(sku_key) < len(specs):  # 如果当前sku信息不完整，则退出
+        return
+
+    # for index, spec in enumerate(specs):
+    #     key = sku_key[:]  # 复制当前的sku的规格键
+    #     options = spec.specificationoption_set.all()  # 该规格的选项
+    #     for option in options:  # 在规格参数sku字典中查找符合当前规格的sku
+    #         key[index] = option.id
+    #         option.sku_id = spec_sku_map.get(tuple(key))
+    #     spec.options = options
+
+    context = {
+        'categories': categories,
+        'goods': goods,
+        'specs': specs,
+        'sku': sku
+    }
+
+    template = loader.get_template('detail.html')
+    html_text = template.render(context)
+    file_path = os.path.join(settings.GENERATED_STATIC_HTML_FILES_DIR, 'goods/' + str(sku_id) + '.html')
     with open(file_path, 'w', encoding='utf-8') as f:
         f.write(html_text)
